bb9051067e615e7c0f81b1dd1c5b509a434ff4fd
Taranveer Singh <taranveersingh93@gmail.com>
2023-09-12 16:39:28 -0400

    Add link to cards

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import fetchArticles from '../../apiCalls';
 import CardContainer from '../CardContainer/CardContainer';
-import Card from '../CardContainer/Card/Card';
 import refineData from '../../helperFunctions';
+import {Route, Routes} from 'react-router-dom'
+import ReactDOM from 'react-dom';
 
 function App() {
   const [newsItems, setNewsItems] = useState([])
@@ -16,12 +17,12 @@ function App() {
     })
   }, [])
 
-  useEffect(() => {
-    // console.log(newsItems)
-  }, [newsItems])
-
   return (
-    <CardContainer newsItems={newsItems}/>
+    <Routes>
+      <Route path='/' element={<CardContainer newsItems={newsItems}/>}/>
+      <Route path='/:id' element={<h1>her</h1>} />
+    </Routes>
+    
   );
 }
 
