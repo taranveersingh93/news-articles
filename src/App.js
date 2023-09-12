0e6e770f0768197434bd103391e1a71ba1f22fd9
Taranveer Singh <taranveersingh93@gmail.com>
2023-09-11 22:31:10 -0400

    Add fetch call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [newsItems, setNewsItems] = useState([])
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/top-headlines?country=us', {
+      method: 'GET',
+      headers: {
+        'x-api-key': process.env.REACT_APP_API_KEY
+      }
+    }).then(res => res.json())
+    .then(data => {console.log(data)})
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<h1>test</h1>
   );
 }
 
